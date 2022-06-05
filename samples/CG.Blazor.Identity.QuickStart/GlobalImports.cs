02a140ac56d05edacd53442a0a28adee187bd26d
martycook <martinc@valornet.com>
2022-06-05 15:42:47 +0000

    CI Updates [skip ci]

--- samples/CG.Blazor.Identity.QuickStart/GlobalImports.cs
+++
@@ -1,5 +0,0 @@
-﻿global using CG.Blazor.Identity.QuickStart.Data;
-global using CG.Blazor.Identity.QuickStart.Services;
-global using Microsoft.AspNetCore.Identity;
-global using Microsoft.AspNetCore.Identity.UI.Services;
-global using Microsoft.EntityFrameworkCore;
